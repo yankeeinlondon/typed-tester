6a286e61025d8ad042e26cdbb4a63c4b89f5fd8d
Ken Snyder <ken@ken.net>
2024-08-10 18:12:21 -0700

    chore: what's the smell? windows i'm afraid

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -8,21 +8,21 @@ console.log(`Building Typed`);
 console.log(`--------------`);
 
 const node_modules_bin = platform === "win32"
-? `node_modules\.bin`
+? `node_modules\\.bin`
 : `./node_modules/.bin`;
 
 const rimraf = join(node_modules_bin, "rimraf");
 const tsup = join(node_modules_bin, "tsup");
 
 const clear_path = platform === "win32"
-? `bin\*`
+? `bin\\*`
 : `./bin/*`;
 
 execSync(`${rimraf} ${clear_path}`);
 console.log(`- cleared ${chalk.bold("bin")} directory of stale artifacts`);
 
 const build_target = platform === "win32"
-  ? `src\typed.ts`
+  ? `src\\typed.ts`
   : `./src/typed.ts`;
 
 if(execSync(`${tsup} ${build_target} --format=esm -d bin --sourcemap`)) {
@@ -34,11 +34,11 @@ if(execSync(`${tsup} ${build_target} --format=esm -d bin --sourcemap`)) {
 
 
 const copy_src = platform === "win32"
-  ? `src\typed`
+  ? `src\\typed`
   : `./src/typed`;
 
 const copy_dest = platform === "win32"
-  ? `bin\typed`
+  ? `bin\\typed`
   : `./bin/typed`;
 
 copyFileSync(copy_src, copy_dest);
