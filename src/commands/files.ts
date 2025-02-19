8beb71d99ec8703579a486a1f940c5750303cd0c
Ken Snyder <ken@ken.net>
2025-02-19 13:00:19 -0800

    chore: added aider script to ask about types

---
+++ src/commands/files.ts
@@ -0,0 +1,16 @@
+import chalk from "chalk";
+import {  } from "node:process";
+import {   } from "pathe";
+import {  
+
+} from "src/ast";
+import { 
+
+} from "src/cache";
+import { AsOption } from "src/cli";
+import {   } from "src/utils";
+
+
+export const file_command = async (opt: AsOption<"source">) => {
+    // TODO
+}
